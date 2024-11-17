417f02f0849b376267fa609276b16523b2cd27c5
Your NameMueez <mueezimran1@gmail.com>
2024-11-17 21:17:47 +0500

    Google front end part

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GoogleOAuthProvider } from '@react-oauth/google'; // Import Google OAuth Provider
+import App from './App.jsx';
+import './index.css';
+
+// Replace with your actual Google Client ID
+const clientId = 'YOUR_GOOGLE_CLIENT_ID';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId={clientId}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
